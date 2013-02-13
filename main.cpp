1ad958113a9cf8cd260372a191b4b4de57ed2595
JT Johnson <DarkFlame57@gmail.com>
2013-02-12 21:21:23 -0800

    Allegro 5 skeleton

--- main.cpp
+++ main.cpp
@@ -1,95 +1,169 @@
+#include <stdio.h>
+#include <allegro5/allegro.h>
 
-#include "allegro5/allegro.h"
-#include "allegro5/allegro_image.h"
-#include "allegro5/allegro_primitives.h"
-#include "allegro5/allegro_native_dialog.h"
+const float FPS = 60;
+const int SCREEN_W = 640;
+const int SCREEN_H = 480;
+const int BOUNCER_SIZE = 32;
+enum MYKEYS {
+   KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
+};
 
-short m_box(const char* message       = "No message given",
-            const char* content_title = "Error",
-            const char* title         = "Error"){
-
-   switch(al_show_native_message_box(al_get_current_display(),
-                                     title,
-                                     content_title,
-                                     message, NULL,
-                                     ALLEGRO_MESSAGEBOX_YES_NO)){
-
-      case 0: return 0; // User pressed the Cancel button
-      case 1: return 1; //  "     "      "    OK     "
-      case 2: return 2; // I don't understand what does this
-
-   }
-}
-
-ALLEGRO_BITMAP *make_err_ima(){
-
-   ALLEGRO_BITMAP *T_img = al_create_bitmap(32,32);
-   if(!T_img)return 0;
-   al_set_target_bitmap(T_img);
-   al_clear_to_color(al_map_rgb(255, 0, 0));
-   al_draw_line( 0,  0, 32, 32, al_map_rgb(255, 255, 255), 1.5);
-   al_draw_line(32,  0,  0, 32, al_map_rgb(255, 255, 255), 1.5);
-   al_set_target_bitmap(al_get_backbuffer(al_get_current_display()));
-   return T_img;
-
-}
-
-ALLEGRO_BITMAP *smart_ima_loader(const char *image_name){
-
-   ALLEGRO_BITMAP *T_img = NULL;
-   T_img = al_load_bitmap(image_name);
-
-   if(!T_img)
-      switch(m_box("There is a proble when loading an image,"
-             "would you like to continue anyway?", image_name)){
-
-         case 0: return NULL; break;
-         case 1: return make_err_ima(); break;
-
-      }
-
-   else
-      return T_img;
-
-}
-
-int main(){
-
+int main(int argc, char **argv)
+{
    ALLEGRO_DISPLAY *display = NULL;
-   ALLEGRO_BITMAP  *image   = NULL;
+   ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+   ALLEGRO_TIMER *timer = NULL;
+   ALLEGRO_BITMAP *bouncer = NULL;
+   float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
+   float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
+   bool key[4] = { false, false, false, false };
+   bool redraw = true;
+   bool doexit = false;
 
-   if(!al_init()){
-      m_box("Problems when initilizing Allegro");
+   if(!al_init()) {
+      fprintf(stderr, "failed to initialize allegro!\n");
+      return -1;
    }
 
-   if(!al_init_image_addon()){
-      m_box("Problems when initilizing the image addon");
+   if(!al_install_keyboard()) {
+      fprintf(stderr, "failed to initialize the keyboard!\n");
+      return -1;
    }
 
-   if(!al_init_primitives_addon()){
-      m_box("Problems when initilizing the primitives addon");
+   timer = al_create_timer(1.0 / FPS);
+   if(!timer) {
+      fprintf(stderr, "failed to create timer!\n");
+      return -1;
    }
 
-   display = al_create_display(800,600);
-
-   if(!display){
-      m_box("Problems when creating the display");
-   }
-   //Since all this initializations are very necessary, there is no much we can do in case they fail
-
-   image = smart_ima_loader("image.png");
-
-   if(!image){
-      return 0;
+   display = al_create_display(SCREEN_W, SCREEN_H);
+   if(!display) {
+      fprintf(stderr, "failed to create display!\n");
+      al_destroy_timer(timer);
+      return -1;
    }
 
-   al_draw_bitmap(image, 200, 200, 0);
+   bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
+   if(!bouncer) {
+      fprintf(stderr, "failed to create bouncer bitmap!\n");
+      al_destroy_display(display);
+      al_destroy_timer(timer);
+      return -1;
+   }
+
+   al_set_target_bitmap(bouncer);
+
+   al_clear_to_color(al_map_rgb(255, 0, 255));
+
+   al_set_target_bitmap(al_get_backbuffer(display));
+
+   event_queue = al_create_event_queue();
+   if(!event_queue) {
+      fprintf(stderr, "failed to create event_queue!\n");
+      al_destroy_bitmap(bouncer);
+      al_destroy_display(display);
+      al_destroy_timer(timer);
+      return -1;
+   }
+
+   al_register_event_source(event_queue, al_get_display_event_source(display));
+
+   al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+   al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+   al_clear_to_color(al_map_rgb(0,0,0));
 
    al_flip_display();
 
-   al_rest(2);
+   al_start_timer(timer);
 
-   al_destroy_bitmap(image);
+   while(!doexit)
+   {
+      ALLEGRO_EVENT ev;
+      al_wait_for_event(event_queue, &ev);
+
+      if(ev.type == ALLEGRO_EVENT_TIMER) {
+         if(key[KEY_UP] && bouncer_y >= 4.0) {
+            bouncer_y -= 4.0;
+         }
+
+         if(key[KEY_DOWN] && bouncer_y <= SCREEN_H - BOUNCER_SIZE - 4.0) {
+            bouncer_y += 4.0;
+         }
+
+         if(key[KEY_LEFT] && bouncer_x >= 4.0) {
+            bouncer_x -= 4.0;
+         }
+
+         if(key[KEY_RIGHT] && bouncer_x <= SCREEN_W - BOUNCER_SIZE - 4.0) {
+            bouncer_x += 4.0;
+         }
+
+         redraw = true;
+      }
+      else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+         break;
+      }
+      else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+         switch(ev.keyboard.keycode) {
+            case ALLEGRO_KEY_UP:
+               key[KEY_UP] = true;
+               break;
+
+            case ALLEGRO_KEY_DOWN:
+               key[KEY_DOWN] = true;
+               break;
+
+            case ALLEGRO_KEY_LEFT:
+               key[KEY_LEFT] = true;
+               break;
+
+            case ALLEGRO_KEY_RIGHT:
+               key[KEY_RIGHT] = true;
+               break;
+         }
+      }
+      else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
+         switch(ev.keyboard.keycode) {
+            case ALLEGRO_KEY_UP:
+               key[KEY_UP] = false;
+               break;
+
+            case ALLEGRO_KEY_DOWN:
+               key[KEY_DOWN] = false;
+               break;
+
+            case ALLEGRO_KEY_LEFT:
+               key[KEY_LEFT] = false;
+               break;
+
+            case ALLEGRO_KEY_RIGHT:
+               key[KEY_RIGHT] = false;
+               break;
+
+            case ALLEGRO_KEY_ESCAPE:
+               doexit = true;
+               break;
+         }
+      }
+
+      if(redraw && al_is_event_queue_empty(event_queue)) {
+         redraw = false;
+
+         al_clear_to_color(al_map_rgb(0,0,0));
+
+         al_draw_bitmap(bouncer, bouncer_x, bouncer_y, 0);
+
+         al_flip_display();
+      }
+   }
+
+   al_destroy_bitmap(bouncer);
+   al_destroy_timer(timer);
+   al_destroy_display(display);
+   al_destroy_event_queue(event_queue);
 
    return 0;
 }
