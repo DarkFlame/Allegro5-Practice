d47711d87e4ad5f296fa61f3ecc5df9d353cc28e
JT Johnson <DarkFlame57@gmail.com>
2013-03-04 15:43:21 -0800

    Mar 4th b.

--- tmxloader.cpp
+++ tmxloader.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <cmath>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_color.h>
 #include "tinyxml.h"
 #include "tinystr.h"
-#include <list>
+#include <stdlib.h>
 #include <map>
 #include <zlib.h>
 
@@ -27,7 +28,7 @@ public:
 
 class TileSet{
 public:
-    char * image_source; //Image filename to pull the tileset from.
+    const char * image_source; //Image filename to pull the tileset from.
     ALLEGRO_BITMAP *image;
     int imgwidth;  //pixels
     int imgheight; //pixels
@@ -39,10 +40,28 @@ public:
     int tileoffsetx; //pixels
     int tileoffsety; //pixels
 
-    const int tilewidth; //pixels
-    const int tileheight;//pixels
-    const int firstgid;  //the first tile id of this tileset. all following tiles will be ++
+    int tilewidth; //pixels
+    int tileheight;//pixels
+    int firstgid;  //the first tile id of this tileset. all following tiles will be ++
     const char * name;
+    void parseXmlElement(TiXmlElement* element){
+        firstgid = atoi(element->Attribute("firstgid"));
+        name = element->Attribute("name");
+        tilewidth = atoi(element->Attribute("tilewidth"));
+        tileheight = atoi(element->Attribute("tileheight"));
+
+        TiXmlElement * imageelem = element->FirstChildElement("image");
+        image_source = imageelem->Attribute("source");
+        imgwidth = atoi(imageelem->Attribute("width"));
+        imgheight= atoi(imageelem->Attribute("height"));
+        const char * transtmp = imageelem->Attribute("trans");
+        if (transtmp)
+            trans = al_color_html(transtmp);
+    }
+    void print(){
+        char * str = "------\ntileset '%s'\n\nfirst tile ID: %i\ntilesize: (%i,%i)\ntileoffset: (%i,%i)\nspacing:%i\nmargin:%i\n\nimage source: '%s'\nimage dimensions: (%i,%i)\n------\n";
+        printf(str, name,firstgid,tilewidth,tileheight,tileoffsetx,tileoffsety,spacing,margin, image_source,imgwidth,imgheight);
+    }
 };
 
 class TileLayer{
@@ -53,38 +72,67 @@ public:
     char * compression; //either gzip or zlib.  If not set, no compression is used.
     char * encoding;    //either base64 or csv. If not set, no encoding is used, plain XML tags.
 
-    int * tiles;
+    int * tiles; // Array of tile IDs
 };
 
 class TileMap{
 public:
+    const char * filename;
     int width; //tiles
     int height;//tiles
     int tilewidth; //pixels
     int tileheight;//pixels
-    std::list<TileLayer> layers; //doubly-linked list
-    std::list<TileSet> tilesets; //doubly-linked list
+    //std::list<TileLayer> layers; //doubly-linked list
+    TileLayer ** layers;
+    //std::list<TileSet> tilesets; //doubly-linked list
+    TileSet ** tilesets;
     ALLEGRO_COLOR backgroundcolor; //not implemented.
 
     TiXmlDocument doc;
 
     // Reads the file in question into TinyXML DOM format
-    void load_from_file(const char * filename){
-        TiXmlDocument doc(filename);
+    void load_from_file(const char * fname){
+        filename = fname;
+        TiXmlDocument doc(fname);
         doc.LoadFile();
 
         TiXmlElement *root = doc.FirstChildElement();
+        width = atoi(root->Attribute("width"));
+        height = atoi(root->Attribute("height"));
+        tilewidth = atoi(root->Attribute("tilewidth"));
+        tileheight= atoi(root->Attribute("tileheight"));
         TiXmlElement *r = root->FirstChildElement("tileset");
-        TileSet tempts = TileSet();
+
+        print();
+        printf("\n");
+        TileSet *tempts;
+
+        int numtilesets;
+        for (numtilesets=0;r!=NULL;numtilesets++){
+            r = r->NextSiblingElement("tileset");
+        }
+
+        TileSet * tilesets[numtilesets];
+        r = root->FirstChildElement("tileset");
         while (r != NULL){
-            // use strtol
-            tempts.firstgid = r->Attribute("firstgid")-'0';
-            tempts.name = r->Attribute("name");
-            tempts.tilewidth = r->Attribute("tilewidth")-'0';
-            tempts.tileheight = r->Attribute("tileheight")-'0';
-            printf("--Tileset--\n%s\n",r->Attribute("name"));
+            numtilesets--;
+            tempts = new TileSet;
+            tempts->parseXmlElement(r);
+            tilesets[numtilesets] = tempts;
+            printf("TILESETLOADED %s",tempts->name);
+            printf("\n\n%i\n\n",numtilesets);
+            //tempts.print();
 
             r = r->NextSiblingElement("tileset");
         }
+        for (int i = 0;i<sizeof(tilesets)/sizeof(tilesets[0]);i++){
+            tilesets[i]->print();
+        }
+    }
+
+    void print(){
+        char * str = "------\nmap %s\n\ndimensions: (%i,%i) (tiles)\ntilesize: (%i,%i)\npixel dimensions: (%i,%i)\n%i layers\n%i tilesets\n------\n";
+        //sizeof(array)/sizeof(array[0])
+        printf(str, filename,width,height,tilewidth,tileheight,width*tilewidth,height*tileheight,sizeof(layers)/sizeof(layers[0]),sizeof(tilesets)/sizeof(tilesets[0]));
     }
 };
